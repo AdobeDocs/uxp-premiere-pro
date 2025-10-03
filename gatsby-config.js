0f85f10bf01449b4f9dba7f545ced8365ffbdfce
Davide Barranca <undavide@gmail.com>
2025-10-03 16:54:17 +0200

    Remove Plugins landing in favour of Getting Started

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         title: "Plugins",
-        path: "plugins/",
+        path: "plugins/getting-started",
       },
       {
         title: "Resources",
