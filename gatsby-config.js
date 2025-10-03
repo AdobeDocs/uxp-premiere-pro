f37cde684ea5332b38d495275a2b2bc55583be61
Davide Barranca <undavide@gmail.com>
2025-10-03 16:56:48 +0200

    Revert change

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         title: "Plugins",
-        path: "plugins/getting-started",
+        path: "plugins/",
       },
       {
         title: "Resources",
